413c7e4ef71216b529f7a26e6f22b448250d620a
Muhammad Anas <daggaanas0888@gmail.com>
2026-05-11 16:06:08 +0530

    Do manual sectioning

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+const btn = document.getElementById('gridbtn')
 
 
-function gridNumber(num){
+function calculateSection(num){
 let square = num * num;
-	numOfDivs = 100 / num
+const numOfDivs = 100 / num
 	for (let i = 0; i < square; i++){
 const div = document.createElement ('div')
 		div.style.height = `${numOfDivs}%`
 		div.style.width = `${numOfDivs}%`
-		div.style.box-sizing = "border-box";
+		div.style.boxSizing = "border-box";
 		div.style.border = "1px solid black"
+	section.append(div)
+
 	}
 }
 
+let section = document.querySelector("#masterGrid")
+calculateSection(16);
+
+
+
+btn.addEventListener("click" =>{
+prompt("enter number of grids ")
+})
